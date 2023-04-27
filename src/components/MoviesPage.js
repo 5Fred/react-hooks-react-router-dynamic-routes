e3f4b0742e8ba3576236d7573cb379be215fbc5a
Fredrick <mwangikingz94@gmail.com>
2023-04-27 15:11:54 +0300

    done

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import MoviesList from "./MoviesList";
+
 
 function MoviesPage({ movies }) {
   return (
     <div>
-      <MoviesList movies={movies} />
+      <MoviesPage movies={movies} />
     </div>
   );
 }
+
 export default MoviesPage;
